c2d510563e02ec5afe6be568e67980a8b9a7d61d
Sultan Kenjeyev <ksula0155@gmail.com>
2017-11-23 11:52:44 +0000

    changed the stirring to use a timer

--- SubSystems/Stirring/Stirring.ino
+++ SubSystems/Stirring/Stirring.ino
@@ -1,36 +1,29 @@
 
-
 // most launchpads have a red LED
-#define READ_PIN P2_0
-
+#include <Timer.h>
+#define ROT_PIN P2_0
+Timer t;                               //instantiate the timer object
+int numrots = 0;
 int rpm = 0;
-// the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   Serial.begin(9600);
   Serial.println("Serial started") ;
-  pinMode(READ_PIN, INPUT);
-
+  t.every(1, takeReading);
+  t.every(1000, calcRPM);
 }
 
 // the loop routine runs over and over again forever:
 void loop() 
 {
-  getRPM(READ_PIN, &rpm);  
-  Serial.print("Status of read pin: ");
-  Serial.println(rpm);
-
+  t.update();
 }
 
-
-void getRPM(int pinNum, int *rmp) {
-  int numrot = 0;
-  
-  for(int i = 0; i < 10; ++i){
-    delay(100);
-    int status = !digitalRead(pinNum);
-    numrot = numrot + status; 
-  }
-  *rmp = numrot * 30; // not 60 because it takes 2  0s for 1 rotation
+void takeReading(){
+  if(!digitalRead(ROT_PIN)) ++numrots;
 }
 
+void calcRPM(){
+  rpm = numrots * 30;
+  numrots = 0; 
+}
