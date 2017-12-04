4c921f048a2513a1da1a8a0662d4819d8bbc10b5
Sultan Kenjeyev <ksula0155@gmail.com>
2017-12-04 16:54:40 +0000

    Stirring is working

--- SubSystems/Stirring/Stirring.ino
+++ SubSystems/Stirring/Stirring.ino
@@ -3,27 +3,42 @@
 #include <Timer.h>
 #define ROT_PIN P2_0
 Timer t;                               //instantiate the timer object
-int numrots = 0;
-int rpm = 0;
+unsigned long int numrots = 0;
+unsigned long int rpm = 0;
+int lastState;
+int measuringInterval = 5000; // measuring interval in millis
+
 void setup() {                
   // initialize the digital pin as an output.
   Serial.begin(9600);
   Serial.println("Serial started") ;
   t.every(1, takeReading);
-  t.every(1000, calcRPM);
+  t.every(measuringInterval, calcRPM);
+  t.every(measuringInterval, printReading);
+  lastState = digitalRead(ROT_PIN);
 }
 
 // the loop routine runs over and over again forever:
 void loop() 
 {
   t.update();
+  
 }
 
 void takeReading(){
-  if(!digitalRead(ROT_PIN)) ++numrots;
+  int state = digitalRead(ROT_PIN);
+  if(state != lastState){
+    ++numrots;
+    lastState = state;
+  }
 }
 
 void calcRPM(){
-  rpm = numrots * 30;
+  rpm = numrots / 4 *(60000 / measuringInterval);
   numrots = 0; 
+}
+
+void printReading(){
+  Serial.print("The RPM is: ");
+  Serial.println(rpm);
 }
