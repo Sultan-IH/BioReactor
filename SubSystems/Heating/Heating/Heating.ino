6e49891cb0dcad77d919f59d1532b7ed2de9a1a2
Shoaib <shoaib.omar.17@ucl.ac.uk>
2017-12-07 10:04:52 +0000

    meh

--- SubSystems/Heating/Heating/Heating.ino
+++ SubSystems/Heating/Heating/Heating.ino
@@ -2,19 +2,24 @@
 #include <msp430.h>
 
 //Definitions and global varaibles
-#define THERMISTORNOMINAL 7990
+#define THERMISTORNOMINAL 8150
 #define BCOEFFICIENT 4220
 #define SERIESRESISTOR 10000
 #define TEMPERATURENOMINAL 25
 
 
 float TempToHold = 25.0;
+float OptimalPh = 5.0;
 short PWM = 0;
+
 
 void setup()
 {
   // put your setup code here, to run once:
+  analogReference(DEFAULT);
   Serial.begin(9600);
+  
+  
 }
 
 String FormatAnalogData(String Name,short data){
@@ -30,14 +35,38 @@ float GetTemp(){
   steinhart = 1.0 / steinhart;// Invert
   return (steinhart - 273.15);//get in C
 }
+
+float GetTempEq(){
+  return (((float)analogRead(2)-548.0)/-3.2);
+}
+
+float GetpH(){
+  // put your main code here, to run repeatedly:
+  float voltage = (float)analogRead(P1_5);
+  //read voltage from pin number 2
+  float x= (voltage/72.59)-2.97;
+  float pH= x;       // convert the voltage into a ph number
+  Serial.print("voltage= ");
+  Serial.println(voltage);
+  Serial.print("pH level= ");                        // printing ph level=
+  Serial.println(pH);   // printing the value of the ph
+  Serial.println("________"); 
+}
 //Self explainatory*
+
+
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   float Temp = GetTemp();
+  float Tempp = GetTempEq();
   float diff = (TempToHold-Temp);
+  if (Tempp < 20){
+    diff = (TempToHold-Tempp);
+  }
   Serial.println(Temp);
+  Serial.println(Tempp);
   if ( diff > 0.5 ){
     //PWM +=1;
     PWM = (short)((diff/15)*255)+110; 
@@ -49,7 +78,11 @@ void loop()
   }/*else if (PWM < 0){
     PWM = 0;
   }*/
-  analogWrite(6,PWM);//Write the pwm
+  analogWrite(14,PWM);//Write the pwm
   Serial.println(PWM);
-  delay(100);
+  Serial.println("----");
+  GetpH();
+  delay(500);
+  analogWrite(10,0);//Write the pwm
+  analogWrite(9,0);//Write the pwm
 }
